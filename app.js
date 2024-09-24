86feda23e8c1f5a553b6b0c21d4535d8807d9d06
Dhaneshwar Kumar <dhannesh@gmail.com>
2024-09-24 16:13:26 +0530

    updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const parent = document.getElementById('parent')
 const root = ReactDOM.createRoot(parent);
 
 const Msg = ()=>{
-    return <h1>Hello from React by Dhanesh</h1>
+    return (
+        <>
+        <h1>Hello from React by Dhanesh</h1>
+        <p>This is simple react app with out CRA/VITE</p>
+        </>
+    )
 }
 root.render(<Msg/>)
